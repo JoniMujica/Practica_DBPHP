e0ee445dc43d31cbb5c51304da36a6b19a52597c
JoniMujica <jonimujica3@hotmail.com>
2021-11-02 14:49:23 -0300

    conexion a la base d edatos

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
     echo "Hola mundo";
+
+    $conexionMySQL = mysqli_connect("127.0.0.1:3306","root","","telefonia"); //host:puerto, user,password,DB
+    mysqli_close($conexionMySQL); //importante cerrar la coneccion DB con esto
 ?>
